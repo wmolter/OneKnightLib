5232f4fc51d4e8723581dd3be6030c35430e962e
WillGamingPC\Will <billobobo@gmail.com>
2022-05-07 23:17:13 +0900

    togglecomponent component

---
+++ ToggleComponents.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace OneKnight {
+    public class ToggleComponents : MonoBehaviour {
+
+        public List<MonoBehaviour> on;
+        public List<MonoBehaviour> off;
+        public bool startOn = true;
+        public UnityEvent onToggle;
+        bool currentState;
+
+        public void Toggle() {
+            currentState = !currentState;
+            foreach(MonoBehaviour turnOn in on) {
+                turnOn.enabled = currentState;
+            }
+            foreach(MonoBehaviour turnOff in off) {
+                turnOff.enabled = !currentState;
+            }
+            onToggle?.Invoke();
+        }
+        // Use this for initialization
+        void Awake() {
+            currentState = !startOn;
+        }
+
+        private void OnEnable() {
+            Toggle();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
